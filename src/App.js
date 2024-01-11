81c8ec1d7f590ed3cc75b7f1bcaafd7dd31ada43
ksskyy <149976697+ksskyy@users.noreply.github.com>
2024-01-10 22:58:01 -0800

    finish header part

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import { Footer } from "./components/Footer";
+import { Showcase } from "./components/Showcase";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>hello</p>
-      </header>
+      <Header />
+      <Showcase />
+      <Footer />
     </div>
   );
 }
