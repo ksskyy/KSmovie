5f10ccdf5e32a2567c77beb6438424d2c7df53df
ksskyy <149976697+ksskyy@users.noreply.github.com>
2024-01-16 23:08:38 -0800

    add favourite button

--- src/components/FavButton.js
+++ src/components/FavButton.js
@@ -1,4 +1,29 @@
 import { HiOutlineHeart } from "react-icons/hi2";
 
-<HiOutlineHeart />;
-<HiMiniHeart />;
+function FavButton({ movieObj, remove, handleFavClick }) {
+  function handleAddFav() {
+    handleFavClick(true, movieObj);
+  }
+
+  function handleRemoveFav() {
+    handleFavClick(false, movieObj);
+  }
+
+  return (
+    <>
+      {remove === false ? (
+        <HiOutlineHeart onClick={handleAddFav} />
+      ) : (
+        // <button onClick={handleAddFav}>Add To Favs</button>
+        <HiMiniHeart onClick={handleRemoveFav} />
+        // <button onClick={handleRemoveFav}>Remove From Favs</button>
+      )}
+    </>
+  );
+}
+
+FavButton.defaultProps = {
+  remove: false,
+};
+
+export default FavButton;
