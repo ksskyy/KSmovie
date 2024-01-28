5cb12908ebb93f71005ae5a0eca9a40f62ebfb10
ksskyy <149976697+ksskyy@users.noreply.github.com>
2024-01-28 13:07:46 -0800

    fix the fav button status in search page

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -4,27 +4,37 @@ import SearchBar from "../components/SearchBar";
 import Card from "../components/Card";
 import { PAGE_NAME } from "../global/globals";
 import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { addFavMovie, deleteFavMovie } from "../features/favs/favsSlice";
 
 const Search = () => {
   const [searchResult, setSearchResult] = useState([]);
   const { query } = useParams();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     document.title = `${PAGE_NAME}-Search`;
   }, []);
+
+  const favourites = useSelector((state) => state.favs.movies) || [];
 
   useEffect(() => {
     if (query) {
       fetchMovies(Requests.fetchSearchResult(query))
         .then((data) => {
           console.log(data.results);
-          setSearchResult(data.results);
+          const resultsWithFavStatus = data.results.map((result) => {
+            const isFav = favourites.some((fav) => fav.id === result.id);
+            return { ...result, isFav };
+          });
+          console.log("Result with Fav Status:", resultsWithFavStatus);
+          setSearchResult(resultsWithFavStatus);
         })
         .catch((err) => {
           return err;
         });
     }
-  }, [query]);
+  }, [query, favourites]);
 
   return (
     <main>
@@ -33,7 +43,9 @@ const Search = () => {
         {searchResult && searchResult.length > 0 ? (
           <div className="movie-list">
             {searchResult.map((result) => {
-              return <Card key={result.id} movie={result} />;
+              return (
+                <Card key={result.id} movie={result} isFav={result.isFav} />
+              );
             })}
           </div>
         ) : (
