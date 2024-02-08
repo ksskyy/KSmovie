2e07165f01e2363ccaa3067f13f3c3e6675c86f8
ksskyy <149976697+ksskyy@users.noreply.github.com>
2024-02-08 13:33:31 -0800

    update upcoming requests

--- src/Requests.js
+++ src/Requests.js
@@ -6,7 +6,7 @@ const Requests = {
   fetchNowPlaying: `${TMDB_BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&sort_by=popularity.desc`,
   fetchPopular: `${TMDB_BASE_URL}/discover/movie?api_key=${API_KEY}&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`,
   fetchTopRated: `${TMDB_BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200&include_adult=false&include_video=false`,
-  fetchUpcoming: `${TMDB_BASE_URL}/movie/upcoming?api_key=${API_KEY}&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`,
+  fetchUpcoming: `${TMDB_BASE_URL}/movie/upcoming?api_key=${API_KEY}&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&region=US|CA`,
   fetchVideos: (movieId) => {
     return `${TMDB_BASE_URL}/movie/${movieId}/videos?api_key=${API_KEY}&language=en-US`;
   },
