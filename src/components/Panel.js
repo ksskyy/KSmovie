f202144d15ed51965f07a3a3d0fbcd752303c312
ksskyy <149976697+ksskyy@users.noreply.github.com>
2024-02-08 20:30:37 -0800

    put all style into the app.css

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,6 +1,5 @@
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
-import "./Panel.css";
 import Movie from "./Movie";
 import { useEffect, useState } from "react";
 import Requests, { fetchMovies } from "../Requests";
